82906d8ce7dfea59e7bb719e4d90426be0a6a51c
JeanCarlosSC <jeancarlosodi@gmail.com>
2022-02-01 22:16:33 -0500

    first commit

---
+++ source/Main.cpp
@@ -0,0 +1,15 @@
+// Main.cpp – initialization using globals
+//
+DataFiles g_DataFiles;
+AudioSystem g_AudioSystem;
+VideoSystem g_VideoSystem;
+
+int main(void)
+{
+    bool done = false;
+    while (! done)
+    {
+        // imagine a cool main loop here
+    }
+    return 0;
+}
